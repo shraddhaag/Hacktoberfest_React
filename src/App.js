ef95c366d418d00c066a52c3e6b96143bfd55da1
Dhruvu <dhruvi.butti@icloud.com>
2018-12-27 00:30:01 +0530

    Display through Render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-const App = () => <h1>Hello</h1>
+//const App = () => <h1>Hello</h1>
 
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
 export default App
+
