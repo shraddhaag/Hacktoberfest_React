df1015b5cdadb4a9a6da04429254049f9f83b946
Dhruvu <dhruvi.butti@icloud.com>
2018-12-27 13:07:58 +0530

    Add props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 
 //const App = () => <h1>Hello</h1>
 
@@ -6,11 +8,21 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <h1>Hello world</h1>
-        <b>Bold</b>
+        <h1>{this.props.txt}
+        {this.props.cat}</h1>
       </div>
     )
   }
 }
+App.PropTypes = {
+  txt:PropTypes.string,
+  cat:PropTypes.number.isRequired
+
+}
+
+App.defaultProps = {
+  cat : 5
+}
 export default App
+
 
