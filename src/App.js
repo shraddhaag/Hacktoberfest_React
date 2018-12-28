dbd812ce4764479069b823a32c815d5bbc1741e7
Dhruvu <dhruvi.butti@icloud.com>
2018-12-28 13:53:48 +0530

    Set state

--- src/App.js
+++ src/App.js
@@ -5,24 +5,29 @@ import PropTypes from 'prop-types'
 //const App = () => <h1>Hello</h1>
 
 class App extends React.Component {
+  constructor(){
+    super(); //this points to App now
+    this.state = {
+      txt : 'this is the state txt',
+      cat : 0
+    }
+  }
+  update( e ){
+    this.setState({txt : e.target.value})
+  }
+  update1( e ){
+    this.setState({cat : e.target.value})
+  }
+
   render(){
     return (
       <div>
-        <h1>{this.props.txt}
-        {this.props.cat}</h1>
+        <input type="text" onChange={this.update.bind(this)}/>
+        <input type="number" onChange={this.update1.bind(this)}/>
+        <h1>{this.state.txt} - {this.state.cat}</h1>
       </div>
     )
   }
 }
-App.PropTypes = {
-  txt:PropTypes.string,
-  cat:PropTypes.number.isRequired
-
-}
-
-App.defaultProps = {
-  cat : 5
-}
 export default App
-
 
