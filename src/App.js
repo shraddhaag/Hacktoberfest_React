7cc5d30cb8c4e6bf8a6cdce6e308e20cf2a5594f
Dhruvu <dhruvi.butti@icloud.com>
2019-01-16 08:38:33 +0530

    Access nested data

--- src/App.js
+++ src/App.js
@@ -5,28 +5,16 @@ import PropTypes from 'prop-types'
 //const App = () => <h1>Hello</h1>
 
 class App extends React.Component {
-  constructor(){
-    super(); //this points to App now
-    this.state = {
-      txt : 'this is the state txt',
-      cat : 0
-    }
-  }
-  update( e ){
-    this.setState({txt : e.target.value})
-  }
-  update1( e ){
-    this.setState({cat : e.target.value})
-  }
-
   render(){
-    return (
-      <div>
-        <input type="text" onChange={this.update.bind(this)}/>
-        <input type="number" onChange={this.update1.bind(this)}/>
-        <h1>{this.state.txt} - {this.state.cat}</h1>
-      </div>
-    )
+    return <Button>I <Heart/> React</Button>
+  }
+}
+
+const Button = (props) => <button>{props.children}</button>
+
+class Heart extends React.Component {
+  render() {
+    return <span> &hearts; </span>
   }
 }
 export default App
